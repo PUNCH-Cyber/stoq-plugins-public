e13373c777c70fe1498f56ad668a92d22808d2a3
Aaron Gee-Clough <geecla@mail.nih.gov>
2015-12-22 14:24:48 -0500

    Minor documentation update

--- worker/fireeye/setup.py
+++ worker/fireeye/setup.py
@@ -3,10 +3,10 @@ from setuptools import setup, find_packages
 setup(
     name="fireeye",
     version="0.1.2",
-    author="Marcus LaFerrera (@mlaferrera)",
+    author="Marcus LaFerrera (@mlaferrera) & Aaron Gee-Clough (@gclef_)",
     url="https://github.com/PUNCH-Cyber/stoq-plugins-public",
     license="Apache License 2.0",
-    description="Saves a file into a directory fireeye monitors via CIFS for analysis",
+    description="Submit a file to a Fireeye MAS/AX via filesystem or API",
     packages=find_packages(),
     include_package_data=True,
 )
