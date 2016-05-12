12aace8cf662e3efc80a92e08810231fadb21b07
g-clef <aaron@avaloncyber.com>
2016-05-12 15:13:47 -0400

    Modifying Elasticsearch connector to allow bulk inserts.

--- connector/elasticsearch/elasticsearch/elasticsearch.py
+++ connector/elasticsearch/elasticsearch/elasticsearch.py
@@ -19,8 +19,9 @@ Overview
 Saves content to an ElasticSearch index
 
 """
-
+import time
 from elasticsearch import Elasticsearch
+from elasticsearch.helpers import bulk
 
 from stoq.plugins import StoqConnectorPlugin
 
@@ -33,6 +34,9 @@ class ElasticSearchConnector(StoqConnectorPlugin):
     def activate(self, stoq):
         self.stoq = stoq
         super().activate()
+        if self.bulk:
+            self.last_commit_time = time.time()
+            self.buffer = []
 
     def query(self, index, query):
         """
@@ -53,6 +57,17 @@ class ElasticSearchConnector(StoqConnectorPlugin):
             self.connect()
 
         return self.es.search(index, q=query)
+
+    def heartbeat(self):
+        super().heartbeat()
+        if not self.bulk:
+            return
+        else:
+            now = time.time()
+            if (now - self.last_commit_time) > self.bulk_interval or len(self.buffer) > self.bulk_size:
+                bulk(client = self.es, actions = self.buffer)
+                self.buffer = []
+
 
     # Primative elasticsearch connector.
     def save(self, payload, **kwargs):
@@ -82,9 +97,17 @@ class ElasticSearchConnector(StoqConnectorPlugin):
         index = kwargs.get('index', self.parentname)
 
         # Insert our data and return our results from the ES server
-        return self.es.index(index=index,
+        if not self.bulk:
+            return self.es.index(index=index,
                              doc_type=index,
                              body=payload)
+        else:
+            action = {"_op_type": "index",
+                      "_index": index,
+                      "_type": index}
+            payload.extend(action)
+            self.buffer.append(payload)
+            return "queued: {}".format(len(self.buffer))
 
     def connect(self):
         """
