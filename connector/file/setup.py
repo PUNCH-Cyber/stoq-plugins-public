f952062872b323278c38d92352a5e8cde4a903d1
Taskr <adam.p.trask@gmail.com>
2016-06-29 19:22:31 -0400

    Added option to File connector for compact output

--- connector/file/setup.py
+++ connector/file/setup.py
@@ -2,8 +2,8 @@ from setuptools import setup, find_packages
 
 setup(
     name="file",
-    version="0.9.6",
-    author="Marcus LaFerrera (@mlaferrera)",
+    version="0.9.7",
+    author="Marcus LaFerrera (@mlaferrera) & Adam Trask (@taskr)",
     url="https://github.com/PUNCH-Cyber/stoq-plugins-public",
     license="Apache License 2.0",
     description="Retrieves and saves content to local disk",
