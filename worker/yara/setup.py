3b0878f8d2c82145530a9eab270064283dff64a9
Marcus LaFerrera <marcus@randomhack.org>
2016-07-05 16:12:36 -0400

    Fix: Installing yara-python from pip rather than github.

--- worker/yara/setup.py
+++ worker/yara/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="yara",
-    version="0.9.3",
+    version="0.9.4",
     author="Marcus LaFerrera (@mlaferrera)",
     url="https://github.com/PUNCH-Cyber/stoq-plugins-public",
     license="Apache License 2.0",
