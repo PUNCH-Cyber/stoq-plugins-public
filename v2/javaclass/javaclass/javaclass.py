c6c83d661f21ddc5aa754fa4318273ad3116be90
Marcus LaFerrera <marcus@randomhack.org>
2018-10-19 22:26:15 -0400

    Add javaclass v2 plugin

---
+++ v2/javaclass/javaclass/javaclass.py
@@ -0,0 +1,60 @@
+#   Copyright 2014-2018 PUNCH Cyber Analytics Group
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+"""
+Overview
+========
+
+Decodes and extracts information from Java Class files
+
+"""
+
+from javatools import unpack_class, ClassUnpackException
+
+from stoq.plugins import WorkerPlugin
+from stoq.exceptions import StoqException
+from stoq import Payload, RequestMeta, WorkerResponse
+
+
+class JavaClassPlugin(WorkerPlugin):
+    def scan(self, payload: Payload, request_meta: RequestMeta) -> WorkerResponse:
+        """
+        Decodes and extracts information from Java Class files
+
+        """
+
+        results = {}
+
+        try:
+            content = unpack_class(payload.content)
+        except ClassUnpackException as err:
+            raise StoqException(f'Unable to parse payload: {err}')
+
+        try:
+            results = {
+                'provided': content.get_provides(),
+                'required': content.get_requires(),
+                'constants': [],
+            }
+            for obj, _, data in content.cpool.pretty_constants():
+                if len(data) <= 6:
+                    continue
+                constants = {}
+                constants['id'] = obj
+                constants['data'] = data
+                results['constants'].append(constants)
+        except Exception as err:
+            raise StoqException(f'Unable to analyze Java Class {err}')
+
+        return WorkerResponse(results)
