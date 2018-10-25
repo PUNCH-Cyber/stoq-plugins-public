64a7def844a9d67c4eae7fda655dd9bf03dfe292
Marcus LaFerrera <marcus@randomhack.org>
2018-10-24 23:38:19 -0400

    Initial rtf worker plugin

---
+++ v2/rtf/rtf/rtf.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+#   Copyright 2014-2018 PUNCH Cyber Analytics Group
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+"""
+Overview
+========
+
+Extract objects from RTF payloads
+
+"""
+
+from oletools import rtfobj
+
+from stoq.plugins import WorkerPlugin
+from stoq import Payload, PayloadMeta, ExtractedPayload, RequestMeta, WorkerResponse
+
+
+class RtfPlugin(WorkerPlugin):
+    def scan(self, payload: Payload, request_meta: RequestMeta) -> WorkerResponse:
+        extracted = []
+        rtf = rtfobj.RtfObjParser(payload.content)
+        rtf.parse()
+
+        for obj_idx, obj in enumerate(rtf.objects):
+            if obj.is_ole:
+                data = obj.oledata
+                meta = PayloadMeta(extra_data={'index': obj_idx})
+            elif obj.is_package:
+                data = obj.olepkgdata
+                meta = PayloadMeta(
+                    extra_data={'index': obj_idx, 'filename': obj.filename}
+                )
+            else:
+                data = obj.rawdata
+                meta = PayloadMeta(extra_data={'index': obj_idx})
+            extracted.append(ExtractedPayload(data, meta))
+        return WorkerResponse(extracted=extracted)
---
+++ v2/rtf/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="rtf",
+    version="2.0.0",
+    author="Marcus LaFerrera (@mlaferrera)",
+    url="https://github.com/PUNCH-Cyber/stoq-plugins-public",
+    license="Apache License 2.0",
+    description="Extract objects from RTF payloads",
+    packages=find_packages(),
+    include_package_data=True,
+)
