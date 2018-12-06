9a78d427bf710af0a98dfb7fee71d5f5cd6cc09e
Marcus LaFerrera <marcus@randomhack.org>
2018-12-06 10:32:24 -0500

    Add ssdeep plugin

---
+++ hash_ssdeep/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ssdeep",
+    version="2.0.0",
+    author="Marcus LaFerrera (@mlaferrera)",
+    url="https://github.com/PUNCH-Cyber/stoq-plugins-public",
+    license="Apache License 2.0",
+    description="Generate a ssdeep hash of payload",
+    packages=find_packages(),
+    include_package_data=True,
+    test_suite='tests',
+)
