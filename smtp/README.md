b6b70f9df63a485e2899fa3bec762b9d97be0d14
Marcus LaFerrera <mal@cragganmore.local>
2018-12-17 10:31:28 -0500

    Add examples for SMTP plugin and additional configuration options

--- smtp/README.md
+++ smtp/README.md
@@ -18,6 +18,24 @@ All options below may be set by:
 
 - `omit_body` [`True`/`False`]: Save body of e-mail (text or html) to the results. If set to `False`, IOCs cannnot be extracted from the body of the e-mail.
 
+- `always_dispatch` [str] = Comma separated list of stoQ plugins to always send extracted attachments to
+
+- `archive_attachments` [`True`/`False`]: Should attachments be archived?
+
 - `extract_iocs` [`True`/`False`]: Use `iocextract` plugin to extract IOCs from objects defined in `ioc_keys`
 
 - `ioc_keys` [str]: Comma separated list of SMTP headers to extract IOCs from. May also include `body` and/or `body_html` to include e-mail body content.
+
+## Usage
+
+### Monitor a Postfix Maildir for incoming e-mails
+
+Monitor `/home/stoq/Maildir/new` for new files using the `dirmon` provider plugin, then scan the SMTP session using the `smtp` plugin. If any attachments are extracted, automatically send them to the `hash` and `yara` plugins and archive them to `/home/stoq/archive` with the `filedir` plugin. Additionally, let's also extract any IOC's found in the SMTP headers and e-mail body using the `iocextract` plugin. Finally, the results will be saved to `/home/stoq/results` using the filedir plugin:
+
+    $ stoq run -P dirmon -A filedir -C filedir -s smtp --plugin-opts \
+        dirmon:source_dir=/home/stoq/Maildir/new \
+        filedir:archive_dir=/home/stoq/archive \
+        filedir:results_dir=/home/stoq/results \
+        smtp:always_dispatch=hash,yara \
+        smtp:archive_attachments=True \
+        smtp:extract_iocs=True
--- smtp/smtp/smtp.py
+++ smtp/smtp/smtp.py
@@ -34,28 +34,58 @@ class SMTPPlugin(WorkerPlugin):
     def __init__(self, config: ConfigParser, plugin_opts: Optional[Dict]) -> None:
         super().__init__(config, plugin_opts)
 
+        self.omit_body = False
+        self.always_dispatch = None
+        self.archive_attachments = True
+        self.extract_iocs = False
+        self.ioc_keys = [
+            'received',
+            'x-orig-ip',
+            'x-originating-ip',
+            'x-remote-ip',
+            'x-sender-ip',
+            'body',
+            'body_html',
+        ]
+
         if plugin_opts and 'omit_body' in plugin_opts:
             self.omit_body = plugin_opts['omit_body']
         elif config.has_option('options', 'omit_body'):
             self.omit_body = config.getboolean('options', 'omit_body')
-        else:
-            self.omit_body = False
+
+        if plugin_opts and 'always_dispatch' in plugin_opts:
+            if isinstance(plugin_opts['always_dispatch'], str):
+                self.always_dispatch = [
+                    x.strip() for x in plugin_opts['always_dispatch'].split(',')
+                ]
+            else:
+                self.always_dispatch = plugin_opts['always_dispatch']
+        elif config.has_option('options', 'always_dispatch'):
+            self.always_dispatch = [
+                x.strip() for x in config.get('options', 'always_dispatch').split(',')
+            ]
+
+        if plugin_opts and 'archive_attachments' in plugin_opts:
+            self.archive_attachments = plugin_opts['archive_attachments']
+        elif config.has_option('options', 'archive_attachments'):
+            self.archive_attachments = config.getboolean(
+                'options', 'archive_attachments'
+            )
 
         if plugin_opts and 'extract_iocs' in plugin_opts:
             self.extract_iocs = plugin_opts['extract_iocs']
         elif config.has_option('options', 'extract_iocs'):
             self.extract_iocs = config.getboolean('options', 'extract_iocs')
-        else:
-            self.extract_iocs = False
 
         if plugin_opts and 'ioc_keys' in plugin_opts:
-            self.ioc_keys = plugin_opts['ioc_keys']
+            if isinstance(plugin_opts['ioc_keys'], str):
+                self.ioc_keys = [x.strip() for x in plugin_opts['ioc_keys'].split(',')]
+            else:
+                self.ioc_keys = plugin_opts['ioc_keys']
         elif config.has_option('options', 'ioc_keys'):
             self.ioc_keys = [
                 x.strip() for x in config.get('options', 'ioc_keys').split(',')
             ]
-        else:
-            self.ioc_keys = []
 
     def scan(self, payload: Payload, request_meta: RequestMeta) -> WorkerResponse:
 
@@ -106,6 +136,7 @@ class SMTPPlugin(WorkerPlugin):
                 else:
                     att_filename = mailpart.sanitized_filename
                 attachment_meta = PayloadMeta(
+                    should_archive=self.archive_attachments,
                     extra_data={
                         'charset': mailpart.charset,
                         'content-description': mailpart.part.get('Content-Description'),
@@ -113,7 +144,8 @@ class SMTPPlugin(WorkerPlugin):
                         'disposition': mailpart.disposition,
                         'filename': att_filename,
                         'type': mailpart.type,
-                    }
+                    },
+                    dispatch_to=self.always_dispatch,
                 )
                 attachment = ExtractedPayload(mailpart.get_payload(), attachment_meta)
                 attachments.append(attachment)
