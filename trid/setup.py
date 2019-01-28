3bb608f1192687596248e15b6f0a5bfd51eeb05c
Marcus LaFerrera <marcus@randomhack.org>
2019-01-28 16:26:53 -0500

    Fix typo (#47)

--- trid/setup.py
+++ trid/setup.py
@@ -6,7 +6,7 @@ setup(
     author="Marcus LaFerrera (@mlaferrera)",
     url="https://github.com/PUNCH-Cyber/stoq-plugins-public",
     license="Apache License 2.0",
-    description="Identify file types from their TrID signatur",
+    description="Identify file types from their TrID signature",
     packages=find_packages(),
     include_package_data=True,
 )
